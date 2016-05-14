5210268810a142630eca05403882e2458178d8aa
Bradley Priest <bradleypriest@gmail.com>
2016-05-14 16:28:35 +0800

    Allow setting extra headers when setting up the connection

--- lib/magento/connection.rb
+++ lib/magento/connection.rb
@@ -10,7 +10,7 @@ module MagentoAPI
 
     def client
       @client ||= XMLRPC::Client.new(config[:host], config[:path], config[:port])
-      @client.http_header_extra = {"accept-encoding" => "identity"}
+      @client.http_header_extra = {"accept-encoding" => "identity"}.merge(config[:http_header_extra] || {})
       @client
     end
 
